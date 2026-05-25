489bda10c3afb53f4efa538b36d5c6790ee92a5b
superharumaki <super.harumaki@gmail.com>
2026-05-25 14:37:43 +0900

    Update convert.py

--- convert.py
+++ convert.py
@@ -2,18 +2,18 @@ import csv
 import json
 
 with open('quotes.csv', encoding='utf-16') as f:
-reader = csv.DictReader(f, delimiter='\t')
-data = list(reader)
+    reader = csv.DictReader(f, delimiter='\t')
+    data = list(reader)
 
 result = []
 
 for row in data:
-result.append({
-'text': row['text'],
-'author': row['author']
-})
+    result.append({
+        'text': row['text'],
+        'author': row['author']
+    })
 
 with open('quotes.json', 'w', encoding='utf-8') as f:
-json.dump(result, f, ensure_ascii=False, indent=2)
+    json.dump(result, f, ensure_ascii=False, indent=2)
 
 print("quotes.json を更新しました")
